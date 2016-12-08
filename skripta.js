32e480aff6b14d055743de84d24bd5f1aa62ba84
Mario <brizic.mario@gmail.com>
2016-12-08 22:37:30 +0100

    Implementiran angular

--- skripta.js
+++ skripta.js
@@ -1,3 +1,14 @@
-var a = 1;
-var b = 2;
-alert(a + b); 
+// dokumentacija na https://docs.angularjs.org/api
+
+var appModule = angular.module("app1", []);
+
+appModule.controller("homeController", function ($scope) {
+	// kod koji se bude nalazio unutar tijela ove funkcije je kod 'homeController' kontrolera
+
+	$scope.ime = "diana";
+
+	$scope.varijabla = "neki tekst za prikazati u viewu.";
+	
+	$scope.isClicked = false;
+
+});
