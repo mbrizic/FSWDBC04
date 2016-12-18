082a625a1c971368f5eb84e958868248f2c6f7bb
Mario <brizic.mario@gmail.com>
2016-12-19 00:00:55 +0100

    HTTP i servisi

--- pages/help/helpController.js
+++ pages/help/helpController.js
@@ -1,4 +1,24 @@
-appModule.controller("helpController", function ($scope) {
+appModule.controller("helpController", function ($scope, $http, dataService) {
+
+	dataService.obradi();
+
+	$scope.unosValuta = "HRK"
+
+	$scope.fetchCurrencyData = function () {
+		//pogledat koji sve http verbs postoje
+		//pogledat query string
+
+		var params = { base: $scope.unosValuta, symbols: ["GBP", "EUR"].join(",") };
+
+		$http
+			.get("http://api.fixer.io/latest", { params: params })
+			.then(function(response){
+				$scope.currency = response.data;
+			})
+			.catch(function(response) {
+				$scope.errorMessage = response.data.error;
+			});
+	};
 
 	$scope.poruka = "bok";
 
@@ -11,4 +31,26 @@ appModule.controller("helpController", function ($scope) {
 		alert("ovo je prikaz");
 	};
 
+	$scope.fetchCurrencyData();
+
 });
+
+
+//DOM API
+document.getElementById("ufdf").innerHTML();
+
+$("#ufdf").css('color', 'red')
+
+
+function reqListener () {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "http://www.example.org/example.txt");
+oReq.send();
+
+$.get("dffdf")
+
+element.prepend("<div>")
---
+++ services/dataService.js
@@ -0,0 +1,10 @@
+appModule.factory("dataService", function () {
+
+	return {
+		obradi: function () {
+
+			console.log('servis pozvan')
+		}
+	};
+
+});
